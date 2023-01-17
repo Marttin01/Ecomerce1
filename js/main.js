cc23407ef587e767ece833ab827d40cadc658f2a
Marttin01 <martin.xd@hotmail.com.ar>
2023-01-17 11:36:06 -0300

    Ecommerce pasos finales

---
+++ js/main.js
@@ -0,0 +1,341 @@
+//PRODUCTOS
+const productos = [
+
+    {
+        id: "silla-gamer-01",
+        titulo: "Silla Gamer 01",
+        imagen: "./img/silla-gamer2.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-02",
+        titulo: "Silla Gamer 02",
+        imagen: "./img/silla-gamer1.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-03",
+        titulo: "Silla Gamer 03",
+        imagen: "./img/silla-gamer3.jpeg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-04",
+        titulo: "Silla Gamer 04",
+        imagen: "./img/silla-gamer4.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-05",
+        titulo: "Silla Gamer 05",
+        imagen: "./img/silla-gamer5.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-06",
+        titulo: "Silla Gamer 06",
+        imagen: "./img/silla-gamer6.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-07",
+        titulo: "Silla Gamer 07",
+        imagen: "./img/silla-gamer7.png",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-08",
+        titulo: "Silla Gamer 08",
+        imagen: "./img/silla-gamer8.png",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-09",
+        titulo: "Silla Gamer 09",
+        imagen: "./img/silla-gamer9.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-10",
+        titulo: "Silla Gamer 10",
+        imagen: "./img/silla-gamer2.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-11",
+        titulo: "Silla Gamer 11",
+        imagen: "./img/silla-gamer2.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-gamer-12",
+        titulo: "Silla Gamer 12",
+        imagen: "./img/silla-gamer2.jpg",
+        categoria: {
+            nombre: "silla-gamer",
+            id: "silla-gamer",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-01",
+        titulo: "Silla Oficina 01",
+        imagen: "./img/sillas-oficina1.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-02",
+        titulo: "Silla Oficina 02",
+        imagen: "./img/sillas-oficina2.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-03",
+        titulo: "Silla Oficina 03 ",
+        imagen: "./img/sillas-oficina3.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-04",
+        titulo: "Silla Oficina 04",
+        imagen: "./img/sillas-oficina4.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-05",
+        titulo: "Silla Oficina 05",
+        imagen: "./img/sillas-oficina4.webp",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-06",
+        titulo: "Silla Oficina 06",
+        imagen: "./img/sillas-oficina6.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-07",
+        titulo: "Silla Oficina 07",
+        imagen: "./img/sillas-oficina7.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-oficina-08",
+        titulo: "Silla Oficina 08",
+        imagen: "./img/sillas-oficina9.jpg",
+        categoria: {
+            nombre: "silla-oficina",
+            id: "silla-oficina",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-01",
+        titulo: "Silla Rustica 01",
+        imagen: "./img/sillas-rustica1.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-02",
+        titulo: "Silla Rustica 02",
+        imagen: "./img/sillas-rustica2.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-03",
+        titulo: "Silla Rustica 03",
+        imagen: "./img/sillas-rustica3.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-04",
+        titulo: "Silla Rustica 04",
+        imagen: "./img/sillas-rustica4.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-05",
+        titulo: "Silla Rustica 05",
+        imagen: "./img/sillas-rustica7.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-06",
+        titulo: "Silla Rustica 06",
+        imagen: "./img/sillas-rustica8.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+    {
+        id: "silla-rustica-07",
+        titulo: "Silla rustica 07",
+        imagen: "./img/sillas-rustica9.jpg",
+        categoria: {
+            nombre: "silla-rustica",
+            id: "silla-rustica",
+        },
+        precio: 10000,
+    },    
+]
+
+const contenedorProducto = document.querySelector("#contenedor-productos");
+
+function cargarProductos ()  {
+
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto-contenedor");
+        div.innerHTML = `
+        <img class="imagen-producto" src="${producto.imagen}" alt="${producto.titulo}" srcset="">
+        <div class="producto-detalles" id="detalles">
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <p class="producto-precio">$${producto.precio}</p>
+            <button class="producto-agregar" id="agregar" id= "${producto.id}">Agregar</button>
+        </div>
+        `;
+
+        contenedorProducto.append(div);
+        
+    })
+}
+
+cargarProductos ();
+
+{/* <div class="producto-contenedor" id="contenedor-producto">
+                <img class="imagen-producto" src="./img/silla-gamer2.jpg" alt="" srcset="">
+                <div class="producto-detalles" id="detalles">
+                    <h3 class="producto-titulo">Silla Gamer 01</h3>
+                    <p class="producto-precio">$10000</p>
+                    <button class="producto-agregar" id="agregar">Agregar</button>
+                </div>
+            </div> */}
+
+
+
+
+
+
+
+//DARK MODE, LIGHT MODE
+const colorModeButton = document.querySelector("#color-icon");
+const grilla = document.querySelector("#grilla");
+const detalles = document.querySelectorAll("#detalles");
+const productoAgregar = document.querySelectorAll("#agregar"); //array hecho por el querySelectorAll
+
+colorModeButton.addEventListener("click", cambiarModoColor);
+
+function cambiarModoColor ()  {
+    grilla.classList.toggle("dark-mode");
+
+    for(let i = 0; i < detalles.length; i++) {
+        detalles[i].classList.toggle("dark-mode")
+    };
+    for(let i = 0; i < productoAgregar.length; i++) {
+        productoAgregar[i].classList.toggle("dark-mode-letter")
+    }
+
+    if(grilla.classList.contains("dark-mode")) {
+        colorModeButton.classList.remove("bi-moon-fill");
+        colorModeButton.classList.add("bi-brightness-high-fill");
+    }else {
+        colorModeButton.classList.remove("bi-brightness-high-fill");
+        colorModeButton.classList.add("bi-moon-fill");
+    }
+}
+
